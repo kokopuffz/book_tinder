d651884603717245eb4a9d7b588d90b8e52abcea
kokopuffz <chanheeya@gmail.com>
2022-04-12 17:04:17 -0500

    railsadmin install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/users', to: 'users#show'
