8e0b620680eec6ad696a6f410abf183cd9b7ba8a
kokopuffz <chanheeya@gmail.com>
2022-04-13 17:31:32 -0500

    matches page up

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,10 +1,67 @@
-class MatchesController < ActionController
+class MatchesController < ApplicationController
+  before_action :authenticate_user!
 
-  before_action :authenticate_user
-  
   def index
-    books_liked = current_user
+    current_user_books_liked = current_user.books.liked.map do |book|
+      book.id
+    end
 
+    @matches = User.all
+    .map do |user|
+      other_user_books_liked = user.books.liked.map do |book|
+        book.id
+      end
+
+      common_books_liked = current_user_books_liked.intersection(other_user_books_liked)
+
+      {
+        id: user.id,
+        email: user.email,
+        number_of_matches: common_books_liked.count
+      }
+    end
+    .reject do |match|
+      match[:id] == current_user.id || match[:number_of_matches] == 0
+    end
+    .sort do |a, b|
+      # 
+      b[:number_of_matches] <=> a[:number_of_matches]
+    end
   end
-
 end
+
+# class MatchesController < ApplicationController
+#   before_action :authenticate_user!
+  
+#   def index
+#     # get index of all books liked by current user
+#     current_user_books_liked = current_user.books.liked.map do |book|
+#       book.id
+#     end
+#     p "likedbooks: "
+#     p current_user_books_liked
+
+#     # get all user's liked books
+#     @matches = User.all.map do |user|
+#       other_user_books_liked = user.books.like.map do |book|
+#         book.id
+#       end
+
+#       common_books = current_user_books_liked.intersection(other_user_books_liked)
+#       { 
+#         id: user.id,
+#         email: user.email
+#       }
+#     end
+    
+#     .reject do |match|
+#       match[:id] == current_user.id || match[:number_of_matches] == 0
+#     end
+
+#     .sort do |a, b|
+#       p a
+#       p b  
+#       b[:number_of_matches] <=> a[:number_of_matches]
+#     end
+#   end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # get '/books/explore', to 'books#explore'
   # controller#action
   get '/profile', to: 'profile#show'
-  get '/users/:id', to: 'users#show'
+  get '/users/:id', to: 'users#show', as: :user
   get '/books/', to: 'books#show'
   post '/books/:id/likes', to: 'likes#create', as: :likes
   get '/matches', to: 'matches#index'
