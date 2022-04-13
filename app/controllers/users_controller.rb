3e5692282fd57809ef15341fa8f6c2b4b7bd0982
kokopuffz <chanheeya@gmail.com>
2022-04-12 21:07:36 -0500

    add show liked

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,17 @@ class UsersController < ApplicationController
   #   @book = Book.order('random()').limit(1).first
   # end
 
-  def show
-    @books = Book.all
+  # show all liked books
+  def show 
+    @user = User.find(params[:id])  
+    @books = @user.books.liked
+    p @books
+    # puts User.liked
+    # @book = Book.order('random()').limit(1).first
+    # @books = User.liked
   end
+
+  # show all disliked books
+
+  # show top 5 users with matched books
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get '/users', to: 'users#show'
+
+  # get '/books/explore', to 'books#explore'
+  # controller#action
+  get '/profile', to: 'profile#show'
+  get '/users/:id', to: 'users#show'
+  # get '/users/books', to: ''
   # Defines the root path route ("/")
   root "home#index"
 end
