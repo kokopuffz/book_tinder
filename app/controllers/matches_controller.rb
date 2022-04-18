e4ed714e976b4605616f1ccd1642c0fb684b97b5
kokopuffz <chanheeya@gmail.com>
2022-04-17 19:45:48 -0500

    clean up

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -30,39 +30,3 @@ class MatchesController < ApplicationController
     end
   end
 end
-
-# class MatchesController < ApplicationController
-#   before_action :authenticate_user!
-  
-#   def index
-#     # get index of all books liked by current user
-#     current_user_books_liked = current_user.books.liked.map do |book|
-#       book.id
-#     end
-#     p "likedbooks: "
-#     p current_user_books_liked
-
-#     # get all user's liked books
-#     @matches = User.all.map do |user|
-#       other_user_books_liked = user.books.like.map do |book|
-#         book.id
-#       end
-
-#       common_books = current_user_books_liked.intersection(other_user_books_liked)
-#       { 
-#         id: user.id,
-#         email: user.email
-#       }
-#     end
-    
-#     .reject do |match|
-#       match[:id] == current_user.id || match[:number_of_matches] == 0
-#     end
-
-#     .sort do |a, b|
-#       p a
-#       p b  
-#       b[:number_of_matches] <=> a[:number_of_matches]
-#     end
-#   end
-# end
--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,21 +1,13 @@
 class ProfileController < ApplicationController
   # layout 'login'
   before_action :authenticate_user!
-  # def show
-  #   @book = Book.order('random()').limit(1).first
-  # end
 
   # show all liked books
   def show 
     @user = current_user 
     @books = @user.books.liked
     @disliked = @user.books.disliked
-    # puts User.liked
-    # @book = Book.order('random()').limit(1).first
-    # @books = User.liked
+    flash.alert = "you just: "
   end
 
-  # show all disliked books
-
-  # show top 5 users with matched books
 end
