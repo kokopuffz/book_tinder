34a686e73563ea5e098845e66e03ab1e94024824
kokopuffz <chanheeya@gmail.com>
2022-04-22 22:32:50 -0500

    cleanup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,10 @@
 class UsersController < ApplicationController
-  # layout 'login'
   before_action :authenticate_user!
-  # def show
-  #   @book = Book.order('random()').limit(1).first
-  # end
 
-  # show all liked books
   def show 
     @user = User.find(params[:id])  
     @books = @user.books.liked
     @disliked_books = @user.books.disliked
   end
-  # show all disliked books
 
-  # show top 5 users with matched books
 end
